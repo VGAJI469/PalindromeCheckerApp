56a3e6dd6fbf087d9ae53f2fe1edf18219434f04
VGAJI469 <gajiwalavansh@gmail.com>
2026-02-26 16:13:27 +0530

    [Vansh] Added UC3-StringReversal

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,21 +1,39 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to the Palidrome Checker App Management System");
-        // Hardcoded string
-        String word = "madam";
-
-        // Reverse the string
+//        // Hardcoded string
+//        String word = "madam";
+//
+//        // Reverse the string
         String reversed = "";
+//
+//        for (int i = word.length() - 1; i >= 0; i--) {
+//            reversed = reversed + word.charAt(i);
+//        }
+//
+//        // Check if palindrome using if-else
+//        if (word.equals(reversed)) {
+//            System.out.println(word + " is a Palindrome.");
+//        } else {
+//            System.out.println(word + " is NOT a Palindrome.");
+//        }
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
-        }
+        System.out.print("Enter a string: ");
+        String original = scanner.nextLine();
+        String LowercasedString = original.toLowerCase();
 
-        // Check if palindrome using if-else
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
-        } else {
-            System.out.println(word + " is NOT a Palindrome.");
+
+        for (int i = LowercasedString.length() - 1; i >= 0; i--) {
+            reversed = reversed + LowercasedString.charAt(i);
         }
+        boolean isPalindrome = LowercasedString.equals(reversed);
+        System.out.println(isPalindrome);
+        scanner.close();
+
+
     }
+
 }
